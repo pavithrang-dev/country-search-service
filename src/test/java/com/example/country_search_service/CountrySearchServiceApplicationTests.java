bc7d133e119771d3337df75653abb817e9c25a1d
Pavithran G <pavithrangangadharan12@gmail.com>
2026-01-28 06:53:42 +0530

    Initial base commit

---
+++ src/test/java/com/example/country_search_service/CountrySearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.country_search_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrySearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
