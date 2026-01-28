f63707341e15c75f181b8b3e6150ad0c8c535c9b
PaviG5 <pavithran.g@dedalus.eu>
2026-01-28 13:13:44 +0530

    docs: add application screenshots for walkthrough

--- src/main/java/com/example/country_search_service/CountrySearchServiceApplication.java
+++ src/main/java/com/example/country_search_service/CountrySearchServiceApplication.java
@@ -8,6 +8,7 @@ public class CountrySearchServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CountrySearchServiceApplication.class, args);
+		System.out.println("Hello world!!!");
 	}
 
 }
